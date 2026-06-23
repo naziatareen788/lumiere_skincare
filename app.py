3ac187d67de328de4b443a8de5f89541427611c2
Nazia Tareen <naziatareen978@gmail.com>
2026-06-23 22:38:33 +0500

    Added products page HTML structure and styling

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Lumière Skincare - Coming Soon"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
